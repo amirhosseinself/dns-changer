0c14ffe63ec318eb044aef52b4a7292ec7817931
amir-9 <aghaamirjan13@gmail.com>
2025-06-29 18:39:46 +0330

    2nd try

--- app/api/dns/[id]/route.ts
+++ app/api/dns/[id]/route.ts
@@ -1,11 +1,13 @@
 import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(
-  req: Request,
-  context: { params: { id: string } }
-) {
-  const { id } = context.params;
+export async function DELETE(req: NextRequest) {
+  const url = new URL(req.url);
+  const id = url.pathname.split("/").pop(); // استخراج id از آدرس URL
+
+  if (!id) {
+    return NextResponse.json({ error: "Missing ID" }, { status: 400 });
+  }
 
   try {
     await prisma.dnsRecord.delete({
