7e2fe209756a7192592a39fee9ae48da512b676e
amir-9 <aghaamirjan13@gmail.com>
2025-08-08 18:59:34 +0330

    fix build err

--- app/api/dns-tags/[id]/route.ts
+++ app/api/dns-tags/[id]/route.ts
@@ -1,24 +1,22 @@
-// app/api/dns-tags/[id]/route.ts
-import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-import { successResponse, errorResponse } from "@/utils/apiResponses";
+import { prisma } from "@/lib/prisma";
+import { errorResponse, successResponse } from "@/utils/apiResponses";
 
 export const DELETE = async (
   req: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) => {
   try {
-    const { id } = params;
+    const { id } = context.params;
 
-    // بررسی وجود تگ
     const existingTag = await prisma.dnsTag.findUnique({ where: { id } });
+
     if (!existingTag) {
       return NextResponse.json(errorResponse([], "Tag not found.", 4), {
         status: 404,
       });
     }
 
-    // حذف تگ
     await prisma.dnsTag.delete({ where: { id } });
 
     return NextResponse.json(
