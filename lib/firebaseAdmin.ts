63ae09f75c3b0d3934448297e2803ac33edbb406
amir-9 <aghaamirjan13@gmail.com>
2025-08-08 22:31:16 +0330

    .

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,13 +1,10 @@
 import admin from "firebase-admin";
-// const { privateKey } = process.env.FIREBASE_PRIVATE_KEY
-//   ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
-//   : { privateKey: "" };
 
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")  // Ensure newlines are correctly formatted
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") // Ensure newlines are correctly formatted
     : "",
 };
 
