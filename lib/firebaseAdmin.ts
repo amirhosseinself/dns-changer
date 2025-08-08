df4602d983f33719ae4e5efc94ade953d977c26c
amir-9 <aghaamirjan13@gmail.com>
2025-08-08 20:01:02 +0330

    test

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,13 +1,15 @@
 import admin from "firebase-admin";
 
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // ✅ this is the fix
+};
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   });
 }
 
-export { admin };
+export default admin;
