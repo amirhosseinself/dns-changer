7618bd6bb7bd4d3c5cbad4e9a72460fafdc10971
amir-9 <aghaamirjan13@gmail.com>
2025-08-09 16:19:18 +0330

    changed login page

--- auth.ts
+++ auth.ts
@@ -43,7 +43,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     },
   },
   pages: {
-    signIn: "/login", // مسیر صفحه لاگین
+    signIn: "/admin/login", // مسیر صفحه لاگین
   },
   ...authConfig,
 });
